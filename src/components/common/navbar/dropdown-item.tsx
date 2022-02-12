f5783e201f965324ac24b8c1c2e541eed28170e4
Muhammad Nauman <muhammadnauman475@gmail.com>
2022-02-12 06:36:52 +0500

    menu fixing

--- src/components/common/navbar/dropdown-item.tsx
+++ src/components/common/navbar/dropdown-item.tsx
@@ -11,18 +11,21 @@ const CustomDropdownItem = ({ data, navState }) => {
         //@ts-ignore
         onClick={() => navState()}
       >
-        <div className="icon icon-sm text-primary">
-          <img src={data.menuFields.icon.sourceUrl} alt={data.label} />
-        </div>
+        {data?.menuFields?.icon && (
+          <div className="icon icon-sm text-primary">
+            <img src={data.menuFields.icon.sourceUrl} alt={data.label} />
+          </div>
+        )}
 
         <div className="ml-4">
           <h6 className="font-weight-bold text-uppercase text-primary mb-0">
             {data.label}
           </h6>
-
-          <p className="font-size-sm text-gray-700 mb-0">
-            {data.menuFields.description}
-          </p>
+          {data?.menuFields?.description && (
+            <p className="font-size-sm text-gray-700 mb-0">
+              {data.menuFields.description}
+            </p>
+          )}
         </div>
       </AnchorLink>
     </>
